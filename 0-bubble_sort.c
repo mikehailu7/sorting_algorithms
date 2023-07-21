e1b637e8a0cbc9a54975de16976c0fa54f922a8d
mikehailu7 <mikehailu7@gmail.com>
2023-07-21 03:54:58 -0700

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,20 +10,18 @@
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t m, idx, 
-	       tmp = 0;
-
-	if (size < 2)
-		return;
-	for (m = 0; m < size; m++)
-		for (idx = 0; idx < size; idx++)
-		{
-			if (array[idx] > array[idx + 1] && array[idx + 1])
-			{
-				tmp = array[idx];
-				array[idx] = array[idx + 1];
-				array[idx + 1] = tmp;
-				print_array(array, size);
-			}
-		}
+    size_t a, index, tmp = 0;
+    if (size < 2)
+        return;
+    for (a = 0; a < size; a++)
+        for (idx = 0; idx < size; idx++)
+        {
+            if (array[idx] > array[idx + 1] && array[idx + 1])
+            {
+                tmp = array[idx];
+                array[idx] = array[idx + 1];
+                array[idx + 1] = tmp;
+                print_array(array, size);
+            }
+        }
 }
