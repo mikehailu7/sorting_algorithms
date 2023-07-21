5f0cdaea2e6860a6dbd1b0bb25a1edb4b3decc20
mikehailu7 <mikehailu7@gmail.com>
2023-07-21 06:14:38 -0700

    sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,50 +2,6 @@
 #include <stdio.h>
 
 /**
- * cocktail_sort_list: This fucntion will implemnt sort.
- * @list: This will represent the list
- * Authors: MikiasHailu and YaredTsgie
- * project: cocktail_sort_list
- */
-void cocktail_sort_list(listint_t **list)
-{
-	listint_t *node;
-	int s_list = 1;
-
-	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
-		return;
-	node = *list;
-	while (s_list == 1)
-	{
-		s_list = 0;
-		while (node->next)
-		{
-			if (node->n > node->next->n)
-			{
-				node = s_list(node->next, list);
-				s_list = 1;
-				print_list(*list);
-			}
-			node = node->next;
-		}
-		if (s_list == 0)
-			break;
-		s_list = 0;
-		while (node->prev)
-		{
-			if (node->n < node->prev->n)
-			{
-				node = s_list(node, list);
-				s_list = 1;
-				print_list(*list);
-			}
-			else
-				node = node->prev;
-		}
-	}
-}
-
-/**
  * swap_node: This function will swap a node.
  * @node: This represent the node
  * @list: This represent the list
@@ -67,4 +23,49 @@ listint_t *swap_node(listint_t *node, listint_t **list)
 	else
 		*list = current;
 	return (current);
+}
+
+/**
+ * cocktail_sort_list: This fucntion will implemnt sort.
+ * @list: This will represent the list
+ * Authors: MikiasHailu and YaredTsgie
+ * project: cocktail_sort_list
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *node;
+	int swap_done = 1;
+
+	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
+		return;
+	node = *list;
+	while (swap_done == 1)
+	{
+		swap_done = 0;
+		while (node->next)
+		{
+			if (node->n > node->next->n)
+			{
+				node = swap_node(node->next, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			node = node->next;
+		}
+		if (swap_done == 0)
+			break;
+		swap_done = 0;
+		while (node->prev)
+		{
+			if (node->n < node->prev->n)
+			{
+				node = swap_node(node, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			else
+				node = node->prev;
+		}
+	}
 }
